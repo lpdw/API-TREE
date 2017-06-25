9d27c2a00c7fa33ae16d1e48cf4801b709e8bf44
Margaux Smits <smits.margaux@gmail.com>
2017-06-25 22:14:59 +0200

    fix stats

--- routes/stats.js
+++ routes/stats.js
@@ -6,30 +6,29 @@ const router = express.Router();
 
 /* GET all inputs page. */
 router.get('/count', (req, res, next) => {
-  const Inputs = db.get().collection('inputs');
-  const Words = db.get().collection('words');
-  // let words = [];
-  // On récupère tous les mots
-  Words.find({}).sort({
-    key: 1
-  }).toArray((err, wordsFound) => {
-    if (err) throw err;
-    // Pour chaque mot on compte le nombre de fois qu'il apparaît dans les inputs
-    wordsFound.forEach((word, index) => {
-      return Inputs.find({
-        words: {
-          $elemMatch: {
-            $eq: word.key
-          }
+const Inputs = db.get().collection('inputs');
+const Words = db.get().collection('words');
+let words = [];
+// On récupère tous les mots
+Words.find({}).sort({
+  key: 1
+}).toArray().then( (wordsFound) => {
+  // Pour chaque mot on compte le nombre de fois qu'il apparaît dans les inputs
+  wordsFound.forEach((word, index) => {
+    Inputs.find({
+      words: {
+        $elemMatch: {
+          $eq: word.key
         }
-      }).count((err, count) => {
-        if (err) throw err;
-        word.count = count;
-
-        if (index === wordsFound.length - 1) return res.status(200).send(wordsFound);
-      });
+      }
+    }).count().then((count) => {
+      word.count = count;
+      words.push(word);
+      if (words.length === wordsFound.length) return res.status(200).send(words);
     });
+
   });
+});
 });
 
 
