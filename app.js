8b0244d208803fdd638f272e41e6ecbaef42a1a4
smargaux <smits.margaux@gmail.com>
2017-06-26 16:21:21 +0200

    fix heroku

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const session = require('express-session');
 const methodOverride = require('method-override');
-const cors = require('cors')
+const cors = require('cors');
 const index = require('./routes/index');
 const inputs = require('./routes/inputs');
 const words = require('./routes/words');
@@ -26,7 +26,7 @@ app.set('view engine', 'ejs');
 app.set('socketio', app.io);
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -40,16 +40,16 @@ app.use('/words', words);
 app.use('/stats', stats);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
   console.log(req);
   console.log(res);
-  var err = new Error('Not Found');
+  const err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use((err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -61,35 +61,35 @@ app.use(function(err, req, res, next) {
   }
   res.send(res.locals.message);
 });
-;
 
-client = mqtt.connect('http://localhost');
 
-client.on('connect', function() {
-  // On souscrit au device arduino (données de test)
-
-  client.subscribe(deviceRoot+"arduino");
-  // A la réception d'un objet, on l'enregistre en base de données
-  // Pour tester l'envoie de message : lancer la commande  (il faut avoir installer mqtt en -general)
-  // mqtt_pub -h "localhost" -t "demo/device/arduino" -m '{"date": "2017-06-08T08:30:19.911Z","inputs": [{"label": "Arduino","value": 0},{"label": "Sentiment","value": 1}]}'
-  client.on('message', function (topic, message) {
-    let input =JSON.parse((message.toString()));
-    // On insert l'input reçut par l'arduino
-    db.get().collection('inputs').insertOne(input)
-    .then(insert => {
-      // On récupère l'objet enregistré
-      db.get().collection('inputs').findOne({_id:insert.insertedId})
-      .then(input => {
-        // On envoie le socket pour actualiser l'arbre
-        app.io.emit("new_inputs",input);
-      });
-        console.log({"insertedCount":insert.insertedCount,"insertedId":insert.insertedId});
-    })
-    .catch(err => {
-      console.log(err);
-    });
-    // client.end();
-  });
-});
+// client = mqtt.connect('http://localhost');
+//
+// client.on('connect', function() {
+//   // On souscrit au device arduino (données de test)
+//
+//   client.subscribe(deviceRoot+"arduino");
+//   // A la réception d'un objet, on l'enregistre en base de données
+//   // Pour tester l'envoie de message : lancer la commande  (il faut avoir installer mqtt en -general)
+//   // mqtt_pub -h "localhost" -t "demo/device/arduino" -m '{"date": "2017-06-08T08:30:19.911Z","inputs": [{"label": "Arduino","value": 0},{"label": "Sentiment","value": 1}]}'
+//   client.on('message', function (topic, message) {
+//     let input =JSON.parse((message.toString()));
+//     // On insert l'input reçut par l'arduino
+//     db.get().collection('inputs').insertOne(input)
+//     .then(insert => {
+//       // On récupère l'objet enregistré
+//       db.get().collection('inputs').findOne({_id:insert.insertedId})
+//       .then(input => {
+//         // On envoie le socket pour actualiser l'arbre
+//         app.io.emit("new_inputs",input);
+//       });
+//         console.log({"insertedCount":insert.insertedCount,"insertedId":insert.insertedId});
+//     })
+//     .catch(err => {
+//       console.log(err);
+//     });
+//     // client.end();
+//   });
+// });
 
 module.exports = app;
