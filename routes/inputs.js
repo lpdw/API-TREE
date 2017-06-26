e1050ba9e890c93b4bb22f79dd36e0f80d221641
smargaux <smits.margaux@gmail.com>
2017-06-26 14:08:01 +0200

    fix nb words

--- routes/inputs.js
+++ routes/inputs.js
@@ -1,4 +1,5 @@
-"use strict";
+
+
 const express = require('express');
 const db = require('../db');
 
@@ -17,19 +18,18 @@ router.get('/', (req, res, next) => {
 
 /* Create a new input page. */
 router.post('/', (req, res, next) => {
-
   const Inputs = db.get().collection('inputs');
   const Words = db.get().collection('words');
-
-  // 1. Premier contrôle nombre de mots
-  if (req.body.words.length !== 6) return res.status(400).send("Vous n'avez pas saisi le bon nombre de mots");
+  const wordsLenght = req.body.word.length;
+  // 1. Premier contrôle nombre de mots (6 ou moins)
+  if (req.body.words.length > 6) return res.status(400).send("Vous n'avez pas saisi le bon nombre de mots");
   // 2. Deuxième contrôle si tous les mots existent
-  Words.find({ key: { $in: req.body.words } }, { _id: 0, word: 0, mood:0 }).count((err, count) => {
+  Words.find({ key: { $in: req.body.words } }, { _id: 0, word: 0, mood: 0 }).count((err, count) => {
     if (err) return res.status(500).send();
-    if (count < 6) return res.status(400).send('Au moins une des clés envoyées ne correspond à aucun mot.');
+    if (count < wordsLenght) return res.status(400).send('Au moins une des clés envoyées ne correspond à aucun mot.');
     const newInput = {
       date: new Date(),
-      words: req.body.words
+      words: req.body.words,
     };
     // On enregistre l'input dans la base de données
     Inputs.insertOne(newInput)
@@ -38,11 +38,11 @@ router.post('/', (req, res, next) => {
       Inputs.findOne({ _id: insert.insertedId })
       .then((input) => {
         // On envoie le socket pour actualiser l'arbre
-        req.app.io.emit('new_inputs',input.words);
+        req.app.io.emit('new_inputs', input.words);
       });
       return res.status(200).send({ insertedCount: insert.insertedCount, insertedId: insert.insertedId });
     })
-    .catch(err => {
+    .catch((err) => {
       console.log(err);
     });
   });
@@ -50,21 +50,17 @@ router.post('/', (req, res, next) => {
 
 // GET previous inputs to sent date
 router.get('/BeforeDate/:date', (req, res, next) => {
-    const Inputs = db.get().collection('inputs');
-    Inputs.find(
-        {
-            date: {
-                "$lte": new Date(req.params.date)
-            }
-        }
-    ).toArray().then(inputs => {
-
-        res.status(200).send({inputs});
-    }).catch(err=>{console.log(err);});
-
+  const Inputs = db.get().collection('inputs');
+  Inputs.find(
+    {
+      date: {
+        $lte: new Date(req.params.date),
+      },
+    },
+    ).toArray().then((inputs) => {
+      res.status(200).send({ inputs });
+    }).catch((err) => { console.log(err); });
 });
-
-
 
 
 module.exports = router;
